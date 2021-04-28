8c833cecdf16d40e6aa8a2bbd578dc409569c6e6
sarangr <sarangr@qburst.com>
2021-04-28 15:18:52 +0530

    Database connection change

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Client } = require('pg')
 const client = new Client({
-  host: 'localhost',
+  host: 'ec2-34-233-0-64.compute-1.amazonaws.com',
   port: 5432,
-  user: 'postgres',
-  database:'short',
-  password: 'sarang@1998',
+  user: 'syiqfqsvbadcaa',
+  database:'damr2i1jhpkk8j',
+  password: '5b7a95a488d3e8aec863714a3985085b0db119fbc1768f1dbde50cc4f82cb37d',
 })
 client.connect(err => {
     if (err) {
